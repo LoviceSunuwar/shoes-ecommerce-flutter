7dfdb4442fe98502ba6094ab374bbcc541c54dbf
Lovice Sunuwar <lovicesunuwar@gmail.com>
2021-03-18 13:33:39 +0545

    s

---
+++ lib/widgets/app.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
